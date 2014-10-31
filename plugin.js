a9a9f830d0a8dd3a97d691cca3093275f678ab80
Desmond Morris <hi@desmondmorris.com>
2014-10-31 11:57:16 -0400

    Moves css to separate file

--- plugin.js
+++ plugin.js
@@ -7,17 +7,9 @@
       'fakeobjects'
     ],
     init: function( editor ) {
-      CKEDITOR.addCss(
-        'img.iframely-embed' +
-        '{' +
-        'background-image: url("http://placehold.it/350x150");' +
-        'background-position: center center;' +
-        'background-repeat: no-repeat;' +
-        'border: 1px solid #a9a9a9;' +
-        'width: 80px;' +
-        'height: 80px;' +
-        '}'
-      );
+  
+      editor.addContentsCss( this.path + 'css/iframely.css' );
+
       editor.addCommand( 'iframely', new CKEDITOR.dialogCommand('iframelyDialog') );
 
       editor.ui.addButton( 'iframely', {
