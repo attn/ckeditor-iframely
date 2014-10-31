7648a5c3e3b6fb9f1723f67bedeb41f257730ab6
Quincy Reid <qreid51@gmail.com>
2014-10-31 07:33:43 -0400

    Adds the variable fakeElement in order to provide a iframely placeholder in the editor.

--- dialogs/iframely.js
+++ dialogs/iframely.js
@@ -2,44 +2,46 @@
   'use strict';
 
   CKEDITOR.dialog.add( 'iframelyDialog', function ( editor ) {
-      return {
-          title: 'Embed via iframely',
-          minWidth: 400,
-          minHeight: 200,
-          contents: [
+    return {
+      title: 'Embed via iframely',
+        minWidth: 400,
+        minHeight: 200,
+        contents: [
+          {
+            id: 'iframely-embed',
+            label: 'Embed via iframely',
+            elements: [
               {
-                  id: 'iframely-embed',
-                  label: 'Embed via iframely',
-                  elements: [
-                    {
-                        type: 'text',
-                        id: 'url',
-                        label: 'URL',
-                        validate: CKEDITOR.dialog.validate.notEmpty( 'URL field cannot be empty.' )
-                    }
-                  ]
+                type: 'text',
+                id: 'url',
+                label: 'URL',
+                validate: CKEDITOR.dialog.validate.notEmpty( 'URL field cannot be empty.' )
               }
-          ],
-          onOk: function() {
-              var dialog = this;
-              var api_key = editor.config.iframely_api_key;
-              var url = encodeURI(dialog.getValueOf( 'iframely-embed', 'url' ));
-              var endpoint = 'http://iframe.ly/api/' + editor.config.iframely_method + '?url=' + url + '&api_key=' + api_key;
-
-              var xmlHttp = null;
-              xmlHttp = new XMLHttpRequest();
-              xmlHttp.open( 'GET', endpoint, false );
-              xmlHttp.send( null );
-
-              // We need error handling
-
-              var json = JSON.parse(xmlHttp.responseText);
-              var embed = editor.document.createElement( 'div' );
-              embed.appendHtml( json.html );
-              editor.insertElement( embed );
-
+            ]
           }
-      };
+        ],
+      onOk: function() {
+        var dialog = this;
+        var api_key = editor.config.iframely_api_key;
+        var url = encodeURI(dialog.getValueOf( 'iframely-embed', 'url' ));
+        var endpoint = 'http://iframe.ly/api/' + editor.config.iframely_method + '?url=' + url + '&api_key=' + api_key;
+
+        var xmlHttp = null;
+        xmlHttp = new XMLHttpRequest();
+        xmlHttp.open( 'GET', endpoint, false );
+        xmlHttp.send( null );
+
+        // We need error handling
+
+        var json = JSON.parse(xmlHttp.responseText);
+        var embed = editor.document.createElement( 'div' );
+        embed.appendHtml( json.html );
+        var placeholder = editor.createFakeElement( embed , 'iframely-embed', 'div', true);
+        editor.insertElement( placeholder );
+
+      }
+
+    };
   });
 
 })();
--- plugin.js
+++ plugin.js
@@ -2,19 +2,34 @@
   'use strict';
 
   CKEDITOR.plugins.add( 'iframely', {
-      icons: 'iframely',
-      init: function( editor ) {
-        editor.addCommand( 'iframely', new CKEDITOR.dialogCommand('iframelyDialog') );
+    icons: 'iframely',
+    requires: [
+      'fakeobjects'
+    ],
+    init: function( editor ) {
+      CKEDITOR.addCss(
+        'img.iframely-embed' +
+        '{' +
+        'background-image: url("http://placehold.it/350x150");' +
+        'background-position: center center;' +
+        'background-repeat: no-repeat;' +
+        'border: 1px solid #a9a9a9;' +
+        'width: 80px;' +
+        'height: 80px;' +
+        '}'
+      );
+      editor.addCommand( 'iframely', new CKEDITOR.dialogCommand('iframelyDialog') );
 
-        editor.ui.addButton( 'iframely', {
-          label: 'Embed via iframely',
-          command: 'iframely',
-          toolbar: 'insert',
-          icon: this.path + "icons/iframely.png"
-        });
+      editor.ui.addButton( 'iframely', {
+        label: 'Embed via iframely',
+        command: 'iframely',
+        toolbar: 'insert',
+        icon: this.path + "icons/iframely.png"
+      });
 
-        CKEDITOR.dialog.add( 'iframelyDialog', this.path + 'dialogs/iframely.js' );
+      CKEDITOR.dialog.add( 'iframelyDialog', this.path + 'dialogs/iframely.js' );
 
-      }
+    }
+
   });
 })();
